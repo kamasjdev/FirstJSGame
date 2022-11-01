e6e3e413bfd553fc68232a23b1eb485538c683d3
KW <k.wojtasinski@wp.pl>
2022-11-01 19:50:39 +0100

    [FirstJSGame] Effect
    Added effect while running and rolling

--- main.js
+++ main.js
@@ -22,17 +22,22 @@ window.addEventListener('load', function(){
             this.input = new InputHandler(this);
             this.ui = new UI(this);
             this.enemies = [];
+            this.particles = [];
+            this.maxParticles = 50;
             this.enemyTimer = 0;
             this.enemyInterval = 1000;
             this.debug = true;
             this.score = 0;
             this.fontColor = 'black';
+            this.player.currentState = this.player.states[0];
+            this.player.currentState.enter();
         }
 
         update(deltaTime) {
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
             
+            // handle enemies
             if (this.enemyTimer > this.enemyInterval) {
                 this.addEnemy();
                 this.enemyTimer = 0;
@@ -40,13 +45,25 @@ window.addEventListener('load', function(){
                 this.enemyTimer += deltaTime;
             }
 
-            this.enemies.forEach(enemy => {
+            this.enemies.forEach((enemy, index) => {
                 enemy.update(deltaTime);
 
                 if (enemy.markedForDeletion) {
-                    this.enemies.splice(this.enemies.indexOf(enemy), 1);
+                    this.enemies.splice(index, 1);
                 }
             });
+
+            // handle particles
+            this.particles.forEach((particle, index) => {
+                particle.update();
+                if (particle.markedForDeletion) {
+                    this.particles.splice(index, 1);
+                }
+            });
+
+            if (this.particles.length > this.maxParticles) {
+                this.particles = this.particles.slice(0, 50);
+            }
         }
 
         draw(context) {
@@ -55,6 +72,10 @@ window.addEventListener('load', function(){
 
             this.enemies.forEach(enemy => {
                 enemy.draw(context);
+            });
+
+            this.particles.forEach(particle => {
+                particle.draw(context);
             });
 
             this.ui.draw(context);
@@ -72,7 +93,6 @@ window.addEventListener('load', function(){
 
     const game = new Game(canvas.width, canvas.height);
     let lastTime = 0;
-    console.log(game);
 
     function animate(timeStamp) {
         const deltaTime = timeStamp - lastTime;
--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 import { keys } from "./keys.js";
-import { Falling, Jumping, Running, Sitting } from "./playerStates.js";
+import { Falling, Jumping, Rolling, Running, Sitting } from "./playerStates.js";
 
 export class Player {
     constructor(game) {
@@ -20,9 +20,7 @@ export class Player {
         this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
-        this.currentState = this.states[0];
-        this.currentState.enter();
+        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game)];
     }
 
     update(input, deltaTime) {
