7635cb197c2997bdfd53938b39b620dd61d6792c
KW <k.wojtasinski@wp.pl>
2022-11-01 17:59:23 +0100

    [FirstJSGame] Enemies

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Background } from './background.js';
+import { ClimbingEnemy, FlyingEnemy, GroundEnemy } from './enemies.js';
 import { InputHandler } from './input.js';
 import { Player } from './player.js';
 
@@ -18,16 +19,47 @@ window.addEventListener('load', function(){
             this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
         }
 
         update(deltaTime) {
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
+            
+            if (this.enemyTimer > this.enemyInterval) {
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime);
+
+                if (enemy.markedForDeletion) {
+                    this.enemies.splice(this.enemies.indexOf(enemy), 1);
+                }
+            });
         }
 
         draw(context) {
             this.background.draw(context);
             this.player.draw(context);
+
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            });
+        }
+
+        addEnemy() {
+            if (this.speed > 0 && Math.random() < 0.5) {
+                this.enemies.push(new GroundEnemy(this));
+            } else if(this.speed > 0) {
+                this.enemies.push(new ClimbingEnemy(this));
+            }
+            this.enemies.push(new FlyingEnemy(this));
         }
     }
 
